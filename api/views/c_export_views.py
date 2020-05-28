0f0460deea590f2a7d3e819d12efa1bdeb1b7181
Kirill <kirill.solovyev.98@gmail.com>
2020-05-28 20:38:51 +0600

    1C integration view

--- api/views/c_export_views.py
+++ api/views/c_export_views.py
@@ -1,6 +1,10 @@
 from rest_framework.views import APIView
+from rest_framework.response import Response
 
 
 class ExportCView(APIView):
     def post(self, request):
         print(request.data)
+
+    def get(self, request):
+        return Response({"status": "OK"})
